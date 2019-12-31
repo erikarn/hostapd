7ef9ebbb300a3028cffcad75eef5d23a3ac7b235
Adrian Chadd <adrian@freebsd.org>
2019-12-31 11:25:44 -0800

    l2_packet: Use PCAP for FreeBSD/DragonFly.
    Signed-off-by: Adrian Chadd <adrian@FreeBSD.org>

--- src/l2_packet/l2_packet_freebsd.c
+++ src/l2_packet/l2_packet_freebsd.c
@@ -8,7 +8,8 @@
  */
 
 #include "includes.h"
-#if defined(__APPLE__) || defined(__GLIBC__)
+#if defined(__FreeBSD__) || defined(__DragonFly__) || \
+    defined(__APPLE__) || defined(__GLIBC__)
 #include <net/bpf.h>
 #endif /* __APPLE__ */
 #include <pcap.h>
